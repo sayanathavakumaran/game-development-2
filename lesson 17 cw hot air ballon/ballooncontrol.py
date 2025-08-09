73dba149a02072e537884fb672108381c1496589
sayanathavakumaran <sagekumar@gmail.com>
2025-08-09 17:56:47 +0100

    Add files via upload

---
+++ lesson 17 cw hot air ballon/ballooncontrol.py
@@ -0,0 +1,10 @@
+import pygame
+from pygame.locals import*
+from time import*
+
+pygame.init()
+screen = pygame.display.set_mode((800,800))
+
+sky = pygame.image.load("sky")
+balloon = pygame.image.load("balloon")
+
